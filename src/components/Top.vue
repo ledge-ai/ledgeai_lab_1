7c0d4f0c600102af13505a3553a14d536fc80c2b
kenji <dgff2079@gmail.com>
2019-03-28 11:43:05 +0900

    topページの変更

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,46 +1,11 @@
 <template>
-  <div class="frame-row" style="font-size: 45px;">
-    <!-- 管理人プロフィール -->
-    <div class="frame-left">
-      <font class="my-name">管理人プロフィール</font>
-      <img src="../assets/kawamura.jpg" width="80%" alt="管理人の写真"><br><br><br>名前：河村健司<br>生年月日：1994年11月13日<br>血液型：O型<br>
-      <p class="side-link">●キリ番BBS●</p>
-      <p class="side-link">●雑談BBS●</p>
-      <p class="side-link">●管理人日記●</p>
-    </div>
-    <!-- メインページ -->
-    <div class="frame-right">
-      <p style="font-weight: bold; margin-top: 40px;">
-        <font style="font-size: 130%;">技術開発室×レッジのホームページへようこそ<br>あなたは
-          <a href="http://www.rays-counter.com/">
-            <img src="http://www.rays-counter.com/d406_f8_022/5ae1ba0388d65/" height="80" alt="アクセスカウンター" border="0">
-          </a>人目のお客様です。
-        </font>
-      </p>
-      <div class="title">
-        <p class="top-title">顔認識AIを使った<span class="saisentan">超最先端</span> Face Competition Game</p>
-      </div>
-      <div style="background: #000; color: #FFF; padding: 5px 0;">
-        <marquee scrollamount="20">
-          <font color="pink">【最新ニュース！】</font>２０１８年５月７日<font color="yellow">技術開発室に、まさ（堀 将記）さんが参加しました</font>♪ <font color="red" class="blinking">New</font>
-          <font color="yellow">レッジに、むろ（室川 武範）さんが参加しました</font>♪ <font color="red" class="blinking">New</font>
-        </marquee>
-      </div>
-
-      <p class="blinking"><br><br>
+  <div style="text-align: center;">
+    <p class="blinking"><br><br>
         <font style="font-weight: bold;" color="red">↓↓↓↓↓↓ ここをクリックして、スタート！！！ ↓↓↓↓↓↓</font><br><br>
-      </p>
-      <router-link to="camera" tag="button">
-        <img class="startbutton" src="../assets/start02.png" alt="レッツスタートボタン" />
-      </router-link>
-
-      <p style="font-size: 100%; font-weight: bold; margin-top: 40px;"><br>
-        <font color="red">レッジと技術開発室のメンバーによる、AI表情測定ゲーム！！！</font><br><br>★ルール★<br>
-        <font color="purple" class="blinking">ルールは簡単！</font><br>
-        <font color="green">お題に合わせた表情を作って、写真を取るだけ！！<br>AIが点数をだして、高い方が勝利するトーナメント戦！！！</font>
-      </p>
-    </div>
-    <audio id="audio" src="/static/hinotori.mp3" autoplay loop></audio>
+    </p>
+    <router-link to="camera" tag="button">
+      <img class="startbutton" src="../assets/start02.png" alt="レッツスタートボタン" />
+    </router-link>
   </div>
 </template>
 
